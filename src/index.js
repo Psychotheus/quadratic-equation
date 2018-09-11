9fd4cd1569f82bdd98b08cb2fabe574451922074
Psychotheus <psychotheus@gmail.com>
2018-09-11 21:13:16 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  let array = equation.split(' ');
+  let a = Number(array[0]);
+  let b = (array[3] == '-')? -Number(array[4]): Number(array[4]);
+  let c = (array[7] == '-')? -Number(array[8]): Number(array[8]);
+  
+   
+
+  console.log(
+    a, b, c
+  );
+
+  let D = b * b - 4 * a * c;
+  let x1 = (-b + Math.sqrt(D)) / (2 * a);
+  let x2 = (-b - Math.sqrt(D)) / (2 * a);
+
+  return [Math.round(x1), Math.round(x2)].sort(function(a, b) {
+    return a - b;
+  });
 }
